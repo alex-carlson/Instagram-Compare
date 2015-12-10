b3b506f51ebb28cf696c3ce3332d394c3e28f4e3
Alex Carlson <alex@acwd.me>
2015-12-09 20:03:07 -0600

    rendering insta data

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,29 @@ var router = express.Router();
 Instagram = require('instagram-node-lib');
 
 Instagram.set('client_id', '08bdf46edcc14d25be3c73c60cdd95b6');
-Instagram.set('client_secret', '');
-Instagram.set('callback_url', 'swag');
+Instagram.set('client_secret', 'be57bad3544941e9a4ba08de3284d4ae');
 
-var instaData = [];
+var instaData1;
+var instaData2;
 
 Instagram.tags.recent({
-  name: 'minneapolis',
+  name: 'food',
   complete: function(data){
-    instaData.push(data);
+    instaData1 = data;
+  }
+});
+
+Instagram.tags.recent({
+  name: 'drink',
+  complete: function(data){
+    instaData2 = data;
   }
 });
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Instagram Compare', inst:  instaData});
+  res.render('index', { title: 'Instagram Compare', ret1: instaData1, ret2: instaData2});
 });
 
 module.exports = router;
